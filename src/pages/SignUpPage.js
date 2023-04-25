e3e5a1759b8e51e2af78d29291e9f5b9c022b757
suelen <sreginaclsilva@gmail.com>
2023-04-25 11:18:16 -0300

    refactor: carregando na pagina de cadastro

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -4,6 +4,7 @@ import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
 import { useState } from "react"
 import env from "../env"
+import { ThreeDots } from "react-loader-spinner"
 
 export default function SignUpPage() {
   const navigate = useNavigate()
@@ -11,6 +12,7 @@ export default function SignUpPage() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [confPassword, setConfPassword] = useState("")
+  const [disabled, setDisabled] = useState(false)
 
   function submitSingUp(e) {
     e.preventDefault()
@@ -18,10 +20,16 @@ export default function SignUpPage() {
       alert("'Senha' e 'Confirme A Senha' precisam ser idênticos.")
       return
     }
+    setDisabled(true)
     const body = { name, email, password }
     axios.post(`${env.REACT_APP_API_URL}/cadastro`, body)
-      .then(() => navigate("/"))
-      .catch(err => alert(err.response.data))
+      .then(() => {
+        alert("Usuário cadastrado com sucesso")
+        navigate("/")
+      })
+      .catch(err => {
+        alert(err.response.data)
+      setDisabled(false)})
   }
   return (
     <SingUpContainer>
@@ -32,29 +40,34 @@ export default function SignUpPage() {
           data-test="name"
           value={name}
           onChange={(e) => setName(e.target.value)}
-          required />
+          required
+          disabled={disabled} />
         <input placeholder="E-mail"
           type="email"
           data-test="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          required />
+          required
+          disabled={disabled} />
         <input placeholder="Senha"
           type="password"
           autocomplete="new-password"
           data-test="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          required />
+          required
+          disabled={disabled} />
         <input placeholder="Confirme a senha"
           type="password"
           autocomplete="new-password"
           data-test="conf-password"
           value={confPassword}
           onChange={(e) => setConfPassword(e.target.value)}
-          required />
+          required
+          disabled={disabled} />
         <button data-test="sign-up-submit" type="submit">
-          Cadastrar
+          {disabled ?
+            <ThreeDots type="ThreeDots" color="#FFF" height={20} width={20} /> : "Cadastrar"}
         </button>
       </form>
 
@@ -71,4 +84,9 @@ const SingUpContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  button{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `

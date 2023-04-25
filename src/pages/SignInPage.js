8ede7e69752ac59e2791a9872fde68707b48ed6e
suelen <sreginaclsilva@gmail.com>
2023-04-25 11:09:48 -0300

    refactor: carregando na pagina de login

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -5,14 +5,17 @@ import { useContext, useState } from "react"
 import axios from "axios"
 import env from "../env"
 import { UserContext } from "../context/logInContext"
+import { ThreeDots } from "react-loader-spinner"
 
 export default function SignInPage() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const navigate = useNavigate()
-  const { onlineUser, setOnlineUser } = useContext(UserContext)
+  const [disabled, setDisabled] = useState(false)
+  const { setOnlineUser } = useContext(UserContext)
   function submitData(e) {
     e.preventDefault()
+    setDisabled(true)
     const body = { email, password }
     axios.post(`${env.REACT_APP_API_URL}/`, body)
       .then(res => {
@@ -21,7 +24,10 @@ export default function SignInPage() {
         setOnlineUser({ token, name })
         navigate("/home")
       })
-      .catch(err => alert(err.response.data))
+      .catch(err => {
+        alert(err.response.data)
+        setDisabled(false)
+      })
   }
   return (
     <SingInContainer>
@@ -32,15 +38,22 @@ export default function SignInPage() {
           data-test="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          required />
+          required
+          disabled={disabled}
+        />
         <input placeholder="Senha"
           type="password"
           autocomplete="new-password"
           data-test="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          required />
-        <button data-test="sign-in-submit" type="submit">Entrar</button>
+          required
+          disabled={disabled}
+        />
+        <button data-test="sign-in-submit" type="submit" disabled={disabled}>
+          {disabled ?
+            <ThreeDots type="ThreeDots" color="#FFF" height={20} width={20} /> : "Entrar"}
+        </button>
       </form>
 
       <Link to="/cadastro">
@@ -56,4 +69,9 @@ const SingInContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  button{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `
